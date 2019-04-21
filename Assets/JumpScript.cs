ce1935147f91db35475b0ce485c1fcfcd2aad2af
VasylTovtKN314 <vasyl.tovt.kn.2016@lpnu.ua>
2019-04-21 16:58:41 +0300

    Initial commit

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float Impulse;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            rb.AddForce(Vector2.up * Impulse, ForceMode2D.Impulse);
+        }
+    }
+}
